26bc395f18e26ddfb924b25f4e928cc500ac095f
Jacob Lee <JLpython@outlook.com>
2021-07-15 14:56:55 -0700

    Session 2

--- dogrun/__main__.py
+++ dogrun/__main__.py
@@ -6,4 +6,14 @@
 
 # TODO: Define default user entries (text, dog sprite)
 
-# TODO: Create screen sequence loop
+# Screen sequence loop
+while True:
+    # TODO: Create opening screen object
+    # TODO: Run opening screen
+
+    # TODO: Rewrite text default
+    # TODO: Rewrite dog sprite default
+
+    # TODO: Create game screen object
+    # TODO: Run game screen
+    pass
